a7500c267c2c633392505fc5860d29743f5ce1c0
Alexander Doroshenko <ado@sitecore.net>
2021-03-28 12:49:28 -0400

    Removed unused files

--- JsonStorage.Web/Controllers/HomeController.cs
+++ JsonStorage.Web/Controllers/HomeController.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace JsonStorage.Web.Controllers
 {
-  public class HomeController : Controller
-  {
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      ViewBag.Title = "Home Page";
+        public ActionResult Index()
+        {
+            Response.RedirectPermanent("https://www.jsonstorage.net/");
 
-      return View();
+            return View();
+        }
     }
-  }
 }
