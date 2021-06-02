c87f4dab05d0949530a1a3f7f1d98f86837a193d
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-06-01 22:35:28 -0400

    Initial commit
    Updated content
    
    Updated content
    
    Updated content
    
    Updated content
    
    Content updates
    
    Updated content

---
+++ src/components/footer/footer.js
@@ -0,0 +1,85 @@
+/** @jsx jsx */
+import { jsx, Heading, Box, Container, Text } from 'theme-ui';
+import { Link } from 'components/link';
+import menuItems from './footer.data';
+export default function Footer() {
+  return (
+    <footer sx={styles.footer}>
+            <Text sx={styles.footer.copyright}>
+        <div className="addthis_inline_share_toolbox"></div>
+      </Text>
+      <Text sx={styles.footer.copyright}>
+        © 2017-2021 Alexander Doroshenko<Link path="https://twitter.com/adoprog" target="_blank">@adoprog</Link>. All Rights Reserved.
+      </Text>
+
+      <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4eafde3b44f5fe2d"></script>
+    </footer>
+  );
+}
+
+const styles = {
+  footer: {
+    container: {
+      width: '100%',
+      alignItems: 'stretch',
+    },
+    footerTopArea: {
+      borderTop: '1px solid',
+      borderTopColor: 'border_color',
+      display: 'flex',
+      flexWrap: 'wrap',
+      pt: [7, null, 8],
+      pb: ['10px', null, null, '20px'],
+      px: [0, null, null, null, 4],
+    },
+    menus: {
+      width: ['50%', null, null, '25%'],
+      display: 'flex',
+      flexDirection: 'column',
+      mb: ['40px', null, null, '60px'],
+    },
+
+    heading: {
+      fontSize: [3, null, null, 4],
+      color: 'text_secondary',
+      fontWeight: '500',
+      mb: [3, 4, 5, null, 6],
+      lineHeight: '1.35',
+    },
+
+    link: {
+      fontSize: ['14px', null, 1],
+      color: 'text',
+      fontWeight: 'body',
+      mb: 2,
+      cursor: 'pointer',
+      transition: 'all 0.35s',
+      display: 'block',
+      textDecoration: 'none',
+      lineHeight: [1.5, null, null, 1.6, 1.8],
+      ':hover': {
+        color: 'primary',
+      },
+      ':last-child': {
+        mb: '0px',
+      },
+    },
+    copyright: {
+      fontSize: ['14px', null, 1],
+      width: '100%',
+      textAlign: 'center',
+      p: ['20px 20px'],
+      backgroundColor: '#FCFDFE',
+      color: 'text',
+      a: {
+        textDecoration: 'none',
+        color: 'inherit',
+        pl: 1,
+        transition: 'all 0.25s',
+        '&:hover': {
+          color: 'primary',
+        },
+      },
+    },
+  },
+};
---
+++ src/sections/package.js
@@ -0,0 +1,125 @@
+/** @jsx jsx */
+import { jsx, Container, Flex } from 'theme-ui';
+import PriceCard from 'components/price-card';
+import SectionHeader from 'components/section-header';
+import { IoIosCheckmarkCircle, IoIosCloseCircle } from 'react-icons/io';
+import PatternBG from 'assets/patternBG.png';
+
+const packages = [
+  {
+    name: 'Free Plan',
+    description: 'For personal projects',
+    buttonText: 'Sign In',
+    points: [
+      {
+        icon: <IoIosCheckmarkCircle />,
+        text: 'Create and manage items, intents, personalization rules.',
+        isAvailable: true,
+      },
+      {
+        icon: <IoIosCheckmarkCircle />,
+        text:
+          'Serve personalized content for static sites, headless sites, mobile apps.',
+        isAvailable: true,
+      },
+      {
+        icon: <IoIosCheckmarkCircle />,
+        text: 'Prebuilt connectors for popular JS frameworks',
+        isAvailable: true,
+      },
+      {
+        icon: <IoIosCloseCircle />,
+        text: 'Collaborate with colleagues in the same workspace',
+        isAvailable: false,
+      },
+      {
+        icon: <IoIosCloseCircle />,
+        text: 'Dedicated Support',
+        isAvailable: false,
+      },
+    ],
+  },
+  {
+    header: 'Recommended',
+    name: 'Premium',
+    description: 'For startup enterprise',
+    priceWithUnit: '$xx.xx/',
+    buttonText: 'Sign In',
+    points: [
+      {
+        icon: <IoIosCheckmarkCircle />,
+        text: 'Create and manage items, intents, personalization rules.',
+        isAvailable: true,
+      },
+      {
+        icon: <IoIosCheckmarkCircle />,
+        text:
+          'Serve personalized content for static sites, headless sites, mobile apps.',
+        isAvailable: true,
+      },
+      {
+        icon: <IoIosCheckmarkCircle />,
+        text: 'Prebuilt connectors for popular JS frameworks',
+        isAvailable: true,
+      },
+      {
+        icon: <IoIosCheckmarkCircle />,
+        text: 'Collaborate with colleagues in the same workspace',
+        isAvailable: true,
+      },
+      {
+        icon: <IoIosCheckmarkCircle />,
+        text: 'Dedicated Support',
+        isAvailable: true,
+      },
+    ],
+  },
+];
+export default function Package() {
+  return (
+    <section id="pricing" sx={styles.pricing}>
+      <Container>
+        <SectionHeader
+          title="Start free, no registration required"
+          slogan="Pricing"
+          isWhite={true}
+        />
+        <Flex
+          sx={{
+            justifyContent: 'center',
+            flexWrap: ['wrap', null, null, 'nowrap'],
+          }}
+        >
+          {packages.map((packageData) => (
+            <PriceCard data={packageData} key={packageData.name} />
+          ))}
+        </Flex>
+      </Container>
+    </section>
+  );
+}
+
+const styles = {
+  pricing: {
+    backgroundColor: 'primary',
+    backgroundImage: `url(${PatternBG})`,
+    backgroundRepeat: `no-repeat`,
+    backgroundPosition: 'center center',
+    backgroundSize: 'cover',
+    py: [8, null, 9, null, null, 10],
+    position: 'relative',
+    '&::before': {
+      position: 'absolute',
+      content: '""',
+      top: 0,
+      right: 0,
+      background:
+        'linear-gradient(-45deg, rgba(42,72,125, 0.3) 25%, transparent 25%, transparent 50%, rgba(42,72,125, 0.3) 50%, rgba(42,72,125, 0.3) 75%, transparent 75%, transparent)',
+      width: '100%',
+      backgroundSize: '350px 350px',
+      height: '100%',
+      opacity: 0.3,
+      zIndex: 0,
+    },
+  },
+};
